f455b98af67cc7e3a7abf90b4cf3154f37c2e496
Tactenator <trevor.mclaurine@gmail.com>
2023-10-06 18:15:55 -0500

    finalizing app

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      'dark-grey': 'hsl(0, 0%, 59%)', 
+      'darker-grey': 'hsl(0, 0%, 17%)'
+    }, 
+    backgroundColor: {
+      'white': "#ffffff", 
+      'black': '#000000'
+    }
   },
   plugins: [],
 }
